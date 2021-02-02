838734b68ac19cd4da0fc9b6a84262f60f22962c
VeganCzech <63636174+VeganCzech@users.noreply.github.com>
2021-02-02 16:50:36 +0100

    update

--- TimeSheetApp/Server/Controllers/UserController.cs
+++ TimeSheetApp/Server/Controllers/UserController.cs
@@ -14,13 +14,48 @@ namespace TimeSheetApp.Server.Controllers
     public class UserController : ControllerBase
     {
         private DatabaseContext _context;
+
         public UserController(DatabaseContext context)
         {
             _context = context;
         }
-        public async Task<ActionResult<List<User>>> GetUsers()
+        [HttpGet("GetAllUsers")]
+        public List<User> GettAllUsers()
         {
-            return NotFound();
+            return _context.Users.ToList();
+        }
+        [HttpGet("Get/{id}")]
+        public async Task<User> GetUserByID(int? id)
+        {
+            User user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+        [HttpPost("Post")]
+        public void Post([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+        [HttpDelete("Delete/{id}")]
+        public async Task<string> DeleteUser(int? id)
+        {
+            User user_del = await GetUserByID(id);
+            if (user_del == null)
+            {
+                return null;
+            }
+            else
+            {
+                _context.Users.Remove(user_del);
+                await _context.SaveChangesAsync();
+                return "User deleted successfully";
+            }
+
         }
     }
 }
